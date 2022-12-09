205796d7d2f09f735952e39ace4c11de537b7be6
Brice G <bricegoudalo@gmail.com>
2022-12-09 22:08:39 +0100

    create about page web and mobile

---
+++ lib/mobile/about_mobile.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class AboutMobile extends StatefulWidget {
+  const AboutMobile({super.key});
+
+  @override
+  State<AboutMobile> createState() => _AboutMobileState();
+}
+
+class _AboutMobileState extends State<AboutMobile> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/web/about_web.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class AboutWeb extends StatefulWidget {
+  const AboutWeb({super.key});
+
+  @override
+  State<AboutWeb> createState() => _AboutWebState();
+}
+
+class _AboutWebState extends State<AboutWeb> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
