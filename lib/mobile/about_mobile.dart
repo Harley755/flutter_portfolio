6666cd35512c07f1237876c29ec3302fbb25c383
Brice G <bricegoudalo@gmail.com>
2022-12-10 07:49:46 +0100

    add drawer

--- lib/mobile/about_mobile.dart
+++ lib/mobile/about_mobile.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+
+import '../component.dart';
 
 class AboutMobile extends StatefulWidget {
   const AboutMobile({super.key});
@@ -11,6 +12,85 @@ class AboutMobile extends StatefulWidget {
 class _AboutMobileState extends State<AboutMobile> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Scaffold(
+        extendBodyBehindAppBar: true, // as App Bar
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+          iconTheme: IconThemeData(
+            size: 35.0,
+            color: Colors.black,
+          ),
+        ),
+        endDrawer: Drawer(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              DrawerHeader(
+                padding: EdgeInsets.only(bottom: 20.0),
+                child: Container(
+                  child: CircleAvatar(
+                    radius: 70.0,
+                    backgroundColor: Colors.white,
+                    backgroundImage: AssetImage('../../assets/photo.png'),
+                  ),
+                ),
+              ),
+              TabsMobile(text: "Home", route: '/'),
+              SizedBox(height: 20.0),
+              TabsMobile(text: "Works", route: '/works'),
+              SizedBox(height: 20.0),
+              TabsMobile(text: "Blogs", route: '/blogs'),
+              SizedBox(height: 20.0),
+              TabsMobile(text: "About", route: '/about'),
+              SizedBox(height: 20.0),
+              TabsMobile(text: "Contact", route: '/contact'),
+              SizedBox(height: 40.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Urllaunch(
+                    svgPath: "../../assets/instagram.svg",
+                    url: "https://www.instagram.com/?hl=fr",
+                  ),
+                  Urllaunch(
+                    svgPath: "../../assets/github.svg",
+                    url: "https://github.com/Harley755",
+                  ),
+                  Urllaunch(
+                    svgPath: "../../assets/twitter.svg",
+                    url: "https://twitter.com/bg_dev2",
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20.0),
+          child: ListView(
+            children: [
+              CircleAvatar(
+                radius: 117.0,
+                backgroundColor: Colors.tealAccent,
+                child: CircleAvatar(
+                  radius: 113.0,
+                  backgroundColor: Colors.black,
+                  child: CircleAvatar(
+                    radius: 110.0,
+                    backgroundColor: Colors.white,
+                    backgroundImage: AssetImage(
+                      "../../assets/photo.png",
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
